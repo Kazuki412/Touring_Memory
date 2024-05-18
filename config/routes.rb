0bfd40c145544e8a5e51c1e99619cf3938aa462e
Kazuki412 <153255954+Kazuki412@users.noreply.github.com>
2024-05-18 03:07:22 +0000

    devise設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   root to: "homes#top"
+  # ユーザー用
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+  #管理者用
+  devise_for :admin, skip: [:passwords], controllers: {
+    registrations: "admin/registrations",
+    sessions: "admin/sessions"
+  }
 end
