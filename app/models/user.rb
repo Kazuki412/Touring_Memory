5f20a22a021eddfc7aba524ad31e66a81fc5d4b7
Kazuki412 <153255954+Kazuki412@users.noreply.github.com>
2024-05-25 03:48:07 +0000

    ログインキーの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:name]
+  
+  validates :email, uniqueness: true
+  validates :name, uniqueness: true
 
   has_many :blogs, dependent: :destroy
   
